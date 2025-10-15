bc755b3bb0976c5ce7092b074de4c5ba37fd233f
Saurabh Agarwal <saurabh.agarwal@zomato.com>
2025-10-15 14:47:09 +0530

    update version 4.2.1

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         // Main Nugget binary
         .binaryTarget(
             name: "Nugget",
-            url: "https://github.com/Zomato-Nugget/nugget-sdk-ios/releases/download/4.2.0-Nugget/Nugget.xcframework.zip",
-            checksum: "619e9855ffee1c0f982c35543712515cd495cfb5cb9f147b5631c8027abe2881"
+            url: "https://github.com/Zomato-Nugget/nugget-sdk-ios/releases/download/4.2.1-Nugget/Nugget.xcframework.zip",
+            checksum: "c7c6bb1ffb47e5fbac4cf08dcda95a74787b7ad253e28d302062880ef9b1e17e"
         ),
         .target(
             name: "NuggetSDK",
